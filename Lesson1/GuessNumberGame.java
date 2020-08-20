cce6360a3993fa0cb0df5a4eb4752ff613c954c2
JVako <69126269+JVako@users.noreply.github.com>
2020-08-20 22:42:31 +0300

    Add files via upload

---
+++ Lesson1/GuessNumberGame.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class GuessNumberGame {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int guessNumber = (int) (Math.random() * 101);
+        System.out.println("Player enter a number : ");
+        do {
+            int playerGuess = scan.nextInt();
+            if (playerGuess < guessNumber) {
+                System.out.println("Player entered number < the number of computer");
+            } else if (playerGuess > guessNumber) {
+                System.out.println("Player entered number > the number of computer");
+            } else if (playerGuess == guessNumber) {
+                System.out.println("Player guessed the number! = " + guessNumber);
+                break;
+            }
+        } while (true);
+    }
+}
