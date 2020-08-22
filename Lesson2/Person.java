0e2b1d42cc20a175abe8baf39d8e92c8e421323e
JVako <69126269+JVako@users.noreply.github.com>
2020-08-22 15:03:56 +0300

    Add files via upload

---
+++ Lesson2/Person.java
@@ -0,0 +1,28 @@
+public class Person {
+    String gender = "man";
+    String name = "Valley";
+    double height = 132,5;
+    double weight = 30,2;
+    int age = 10;
+    int sit, go, run, speak, learnJava;
+
+    public void Sit(int newValue) {
+        sit = newValue;
+    }
+
+    public void Go(int newValue) {
+        go = newValue;
+    }
+
+    public void Run(int newValue) {
+        run = newValue;
+    }
+
+    public void Speak(int newValue) {
+        speak = newValue;
+    }
+
+    public void LearnJava(int newValue) {
+        learnJava = newValue;
+    }
+}
