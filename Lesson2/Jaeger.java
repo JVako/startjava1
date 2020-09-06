f6141cc10332363e957228df84eb730ab89c0a7b
JVako <69126269+JVako@users.noreply.github.com>
2020-09-07 01:16:02 +0300

    Add files via upload

---
+++ Lesson2/Jaeger.java
@@ -0,0 +1,83 @@
+public class Jaeger {
+    private String name = "Brawler Yukon";
+    private String model = "Mark-1";
+    private int launch = 2015;
+    private String origin = "United States of America";
+    private String pattern = "Brawler Yukon is a Mark-1 prototype Jaeger";
+    private String placement = "Vancouver";
+    private String jaegSpecifications = "Jaeger";
+    private String combSpecifications = "Out-of-Universe information";
+    private String status = "Destroyed";
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setLaunch(int launch) {
+        this.launch = launch;
+    }
+
+    public int getLaunch() {
+        return launch;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setPattern(String pattern) {
+        this.pattern = pattern;
+    }
+
+    public String getPattern() {
+        return pattern;
+    }
+
+    public void setPlacement(String placement) {
+        this.placement = placement;
+    }
+
+    public String getPlacement() {
+        return placement;
+    }
+
+    public void setJaegSpecifications(String jaegSpecifications) {
+        this.jaegSpecifications = jaegSpecifications;
+    }
+
+    public String getJaegSpecifications() {
+        return jaegSpecifications;
+    }
+
+    public void setCombSpecifications(String combSpecifications) {
+        this.combSpecifications = combSpecifications;
+    }
+
+    public String getCombSpecifications() {
+        return combSpecifications;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+}
