3f6160a39086ad3fd16e78263e9790e72b0b9427
JVako <69126269+JVako@users.noreply.github.com>
2020-09-27 22:52:46 +0300

    Add files via upload

---
+++ Lesson2/CalculatorTest.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class CalculatorTest {
+    Scanner scanner = new Scanner(System.in);
+    private int x;
+    private int y;
+    private char sign;
+
+    public static void main(String[] args) {
+        CalculatorTest calcTest = new CalculatorTest();
+        Calculator calc = new Calculator();
+        Scanner scanner = new Scanner(System.in);
+        do {
+            calcTest.inputMathExpression();
+            calc.calculate(calcTest.x, calcTest.y, calcTest.sign);
+            System.out.println(" - it is result");
+            if (!calcTest.wantContinue()) {
+                break;
+            }
+        } while (true);
+    }
+
+    public void inputMathExpression() {
+        System.out.println("Enter first number: ");
+        x = scanner.nextInt();
+        System.out.println("Enter operation: ");
+        sign = scanner.next().charAt(0);
+        System.out.println("Enter second number: ");
+        y = scanner.nextInt();
+    }
+
+    public boolean wantContinue() {
+        boolean runAction = true;
+        scanner = new Scanner(System.in);
+        String answer;
+        do {
+            System.out.println("Are you want to continue?");
+            answer = scanner.nextLine();
+            if (answer.equals("yes")) {
+                runAction = true;
+                return runAction;
+            }
+            if (answer.equals("no")) {
+                runAction = false;
+            } else {
+                continue;
+            }
+        } while (runAction);
+        return runAction;
+    }
+}
