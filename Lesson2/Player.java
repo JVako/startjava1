cc0082a28198b19435deac9e297653254b06543c
JVako <69126269+JVako@users.noreply.github.com>
2020-10-11 02:05:28 +0300

    Delete Player.java

--- Lesson2/Player.java
+++
@@ -1,9 +0,0 @@
-public class Player {
-    public String name;
-    public int number;
-
-    public Player(String name, int number) {
-        this.name = name;
-        this.number = number;
-    }
-}
