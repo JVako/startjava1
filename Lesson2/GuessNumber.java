207b71b46a06f71d78d9f1617d53e42da000508f
JVako <69126269+JVako@users.noreply.github.com>
2020-10-25 04:01:24 +0300

    Add files via upload

---
+++ Lesson2/GuessNumber.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class GuessNumber {
+    private static int computerNumber;
+
+    public GuessNumber() {
+        this.computerNumber = computerNumber;
+    }
+
+    public void setComputerNumber() {
+        this.computerNumber = computerNumber;
+    }
+
+    public static int getComputerNumber() {
+        return computerNumber;
+    }
+
+    public static int generateNumber() {
+        Scanner scan = new Scanner(System.in);
+        int computerNumber = (int) (Math.random() * 101);
+        return computerNumber;
+    }
+}
---
+++ Lesson2/GuessNumberTest.java
@@ -0,0 +1,78 @@
+import java.util.Scanner;
+
+public class GuessNumberTest {
+    Scanner scan = new Scanner(System.in);
+    Player player1 = new Player();
+    Player player2 = new Player();
+    String nameScanPlayer1 = player1.getName();
+    String nameScanPlayer2 = player2.getName();
+    int numberScanPlayer1 = player1.getNumber();
+    int numberScanPlayer2 = player2.getNumber();
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        GuessNumberTest guessNumberTest = new GuessNumberTest();
+        GuessNumber guessNumber = new GuessNumber();
+        do {
+            System.out.println("GENERATE NUMBER " + GuessNumber.generateNumber());
+            guessNumberTest.enterName();
+            while (true) {
+                guessNumberTest.enterNumberPlayer1();
+                guessNumberTest.enterNumberPlayer2();
+                continue;
+            }
+        } while (guessNumberTest.wantContinue());
+    }
+
+    public void enterName() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the name of 1 player ");
+        nameScanPlayer1 = scan.nextLine();
+        System.out.println("Welcome " + nameScanPlayer1 + ". This is 1 player.");
+        System.out.println("Enter the name of 2 player ");
+        nameScanPlayer2 = scan.nextLine();
+        System.out.println("Welcome " + nameScanPlayer2 + ". This is 2 player.");
+    }
+
+    public void enterNumberPlayer1() {
+        do {
+            System.out.println(nameScanPlayer1 + " - 1 player enter number.");
+            numberScanPlayer1 = scan.nextInt();
+            if (numberScanPlayer1 < GuessNumber.getComputerNumber()) {
+                System.out.println("Player 1 entered number < the number of computer.");
+            } else if (numberScanPlayer1 > GuessNumber.getComputerNumber()) {
+                System.out.println("Player 1 entered number > the number of computer");
+            } else if (numberScanPlayer1 == GuessNumber.getComputerNumber()) {
+                System.out.println("Yes! Right! Player1 " + numberScanPlayer1 + " You guessed the number!");
+                break;
+            }
+        } while (!true);
+    }
+
+    public void enterNumberPlayer2() {
+        do {
+            System.out.println(nameScanPlayer2 + " - 2 player enter number.");
+            numberScanPlayer2 = scan.nextInt();
+            if (numberScanPlayer2 < GuessNumber.getComputerNumber()) {
+                System.out.println("Player 2 entered number < the number of computer.");
+            } else if (numberScanPlayer2 > GuessNumber.getComputerNumber()) {
+                System.out.println("Player 2 entered number > the number of computer");
+            } else if (numberScanPlayer2 == GuessNumber.getComputerNumber()) {
+                System.out.println("Yes! Right! Player2 " + numberScanPlayer2 + " You guessed the number!");
+                break;
+            }
+        } while (!true);
+    }
+
+    public boolean wantContinue() {
+        boolean runAction = true;
+        String answer = " ";
+        do {
+            System.out.println("Are you want to continue?");
+            answer = scan.nextLine();
+        } while (!answer.equals("yes") && !answer.equals("no"));
+        return answer.equals("yes");
+    }
+}
+
+
