4368e10f043d7f737810d4e463627826e8127d38
JVako <69126269+JVako@users.noreply.github.com>
2020-10-25 13:10:11 +0300

    Add files via upload

---
+++ Lesson2/Player.java
@@ -0,0 +1,26 @@
+public class Player {
+    private String name;
+    private int number;
+
+    public Player() {
+        String name = "Bob";
+        this.name = name;
+        this.number = number;
+    }
+
+    public void setName() {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setNumber() {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+}
