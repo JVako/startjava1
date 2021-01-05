df7168b641af75e98522b362991d4f53bb798db5
Юлия Варочко <69126269+JVako@users.noreply.github.com>
2021-01-06 01:59:24 +0300

    Add files via upload

---
+++ Lesson2/GuessNumberTest.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class GuessNumberTest {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Player 1 enter your name");
+        Player player1 = new Player(scan.nextLine());
+        System.out.println("Player 2 enter your name");
+        Player player2 = new Player(scan.nextLine());
+        GuessNumber guessNumber = new GuessNumber(player1, player2);
+        do {
+            guessNumber.startGame();
+        } while (isNext());
+    }
+
+    private static boolean isNext() {
+        Scanner scan = new Scanner(System.in);
+        String answer;
+        do {
+            System.out.println("Are you want to continue?");
+            answer = scan.nextLine();
+        } while (!answer.equals("yes") && !answer.equals("no"));
+        return answer.equals("yes");
+    }
+}
+
