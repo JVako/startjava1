e290dca62a1443491279c11ac9c1e0430a5dfa45
Юлия Варочко <69126269+JVako@users.noreply.github.com>
2021-02-07 23:27:33 +0300

    Add files via upload

---
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class CalculatorTest {
+    Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        CalculatorTest calcTest = new CalculatorTest();
+        Calculator calc = new Calculator();
+        do {
+            calc.splitExpression(calcTest.inputMathExpression());
+            calc.calculate(0);
+            System.out.println(" - it is result");
+        } while (calcTest.isNext());
+    }
+
+    public String inputMathExpression() {
+        System.out.println("Enter a mathematical expression (x + y)");
+        return scanner.nextLine();
+    }
+
+    public boolean isNext() {
+        String answer;
+        do {
+            System.out.println("Are you want to continue?");
+            answer = scanner.nextLine();
+        } while (!answer.equals("yes") && !answer.equals("no"));
+        return answer.equals("yes");
+    }
+}
