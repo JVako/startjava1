58d0c863e0483c21a7b3e8c39c751fbc26209473
Юлия Варочко <69126269+JVako@users.noreply.github.com>
2021-02-11 00:57:17 +0300

    Add files via upload

---
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -0,0 +1,39 @@
+public class Calculator {
+    private int x;
+    private int y;
+    private char sign;
+    private int result;
+
+    public void splitExpression(String expression) {
+        String[] exprArr = expression.split(" ");
+        x = Integer.parseInt(exprArr[0]);
+        y = Integer.parseInt(exprArr[2]);
+        sign = exprArr[1].charAt(0);
+    }
+
+    public int calculate() {
+        switch (sign) {
+            case '+':
+                return x + y;
+            case '-':
+                return x - y;
+            case '*':
+                return x * y;
+            case '/':
+                return x / y;
+            case '%':
+                return x % y;
+            case '^':
+                return (int) Math.pow(x, y);
+            case 'q':
+                return (int) Math.sqrt(x);
+            case 'b':
+                return (int) Math.cbrt(x);
+            case 'f':
+                return (int) Math.floor(x);
+            case 'c':
+                return (int) Math.ceil(x);
+        }
+        return result;
+    }
+}
